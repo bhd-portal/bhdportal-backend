70410d5490c544e83ff9439623ffe6ac85f3b90f
Gil Shafat <gish43@gmail.com>
2019-06-20 10:50:46 +0300

    Added the schema.

--- models/CommanderWords.js
+++ models/CommanderWords.js
@@ -3,7 +3,14 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const CommanderWordsSchema = new Schema({
-
+    title: {
+        type: String,
+        required: true
+    },
+    content: {
+        type: String,
+        required: true
+    }
 });
 
 module.exports = CommanderWords = mongoose.model(
