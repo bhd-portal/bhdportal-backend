3cc2f341816742403410e8b4f4cf38b00e636117
Gil Shafat <gish43@gmail.com>
2019-06-20 11:32:13 +0300

    Added add word handler.

---
+++ controllers/CommanderWords.js
@@ -0,0 +1,17 @@
+
+const CommanderWords = require('../models/CommanderWords.js');
+
+const addCommanderWord = (req, res) => {
+    const body = req.body;
+    const commanderWords = new CommanderWords(body);
+
+    if (!body.title || !body.content) {
+        res.status(422).send('title or content are missing.');
+    }
+
+    commanderWords.save(err => {
+        res.sendStatus(500);
+    });
+
+    res.sendStatus(200);
+};
